b4303a73677e83c1acaacaac73f6a8bfb55dc305
Abdi Hassan Noor <abdihassannoor@Abdis-MBP.home>
2020-03-05 20:45:55 +0200

    fetched the api using axios

--- gif-app/src/App.js
+++ gif-app/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 import SearchForm from './Components/SearchForm';
 import GifList from './Components/GifList';
@@ -7,9 +8,23 @@ export default class App extends Component {
 
     constructor() {
         super();
+        this.state = {
+            gifs: []
+        };
     }
-
+    componentDidMount() {
+        axios.get('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+            .then(res => {
+                this.setState({
+                    gifs: res.data
+                });
+            })
+            .catch(err => {
+            console.log('Error fetching and parsing data', err)
+        });
+    }
     render() {
+        console.log(this.state.gifs);
         return (
             <div>
                 <div className="main-header">
